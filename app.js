cab962aea7f0fe526b178736fe2d1c90ba7ab419
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-06-24 15:01:48 +0200

    Iteration 1

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/views/home-view.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + '/views/works.html')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
